2675cd37b696036e50dd41db4876431b3c1912e5
oomusou <oomusou@gmail.com>
2018-11-13 13:28:17 +0800

    v-model with data

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,16 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'quotes': 'off',
+    'max-len': 'off',
+    'func-names': 'off',
+    'arrow-parens': 'off',
+    'no-param-reassign': 'off',
+    'no-return-assign': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'space-before-function-paren': 'off',
+    'no-shadow': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
